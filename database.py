716d4563ede7382ab51edf968b749ed79aab3d51
Victoroide <cvictorhugo39@gmail.com>
2024-12-07 13:27:57 -0400

    Fix connection string formatting in database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import pyodbc
 def get_connection():
     conn = pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=DESKTOP-MNBF1KF\\SQLEXPRESS;'
+        r'SERVER=DESKTOP-MNBF1KF\SQLEXPRESS;'
         'DATABASE=dataMart;'
         'Trusted_Connection=yes;'
     )
